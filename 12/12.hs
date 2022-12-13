d37167d5a9d0ce184b19865222ab93402b039e7f
l <info@datas.ink>
2022-12-13 12:27:43 +0000

    12

---
+++ 12/12.hs
@@ -0,0 +1,43 @@
+import Control.Arrow
+import Control.Monad
+import Data.Bool
+import Data.IntSet (IntSet)
+import Data.List
+import System.IO
+import qualified Data.IntSet as IntSet
+
+type Point = (Int, Int)
+
+charLocations :: Char -> [[Char]] -> [(Int, Int)]
+charLocations c xs = [(x,y) | (y,row) <- zip [0..] xs, x <- elemIndices c row]
+
+firstCharLocation :: Char -> [[Char]] -> (Int, Int)
+firstCharLocation = (head .) . charLocations
+
+stripBeginAndEnd :: [[Char]] -> [[Char]]
+stripBeginAndEnd = map (map swap) where
+    swap 'S' = 'a'
+    swap 'E' = 'z'
+    swap other = other
+
+-- Hash for use of IntSet
+k :: Point -> Int
+k (x,y)
+    | x' >= y' = x'*x' + x' + y'
+    | otherwise = x' + y'*y' where
+        x' = bool (-2*x - 1) (2*x) (x >= 0)
+        y' = bool (-2*y - 1) (2*y) (y >= 0)
+
+bfs :: IntSet -> [[Char]] -> Point -> [(Int, Point)] -> Int
+bfs visited heightMap to (next:toVisit)
+    | snd next == to = fst next
+    | IntSet.member kNext visited = bfs visited heightMap to toVisit
+    | otherwise = bfs (IntSet.insert kNext visited) heightMap to (toVisit ++ newPoints) where
+        kNext = k $ snd next
+        newPoints = zip (repeat (1 + fst next)) $ filter possible $ map ($ snd next) [first (+1), first (subtract 1), second (+1), second (subtract 1)]
+        possible (x, y) = (x >= 0) && (x < length (head heightMap)) && (y >= 0) && (y < length heightMap) && ((fromEnum (heightMap!!y!!x) - fromEnum (heightMap!!snd (snd next)!!fst (snd next))) <= 1) && IntSet.notMember (k (x,y)) visited
+
+solution1 = bfs IntSet.empty <$> stripBeginAndEnd <*> firstCharLocation 'E' <*> return . (0,) . firstCharLocation 'S'
+solution2 = bfs IntSet.empty <$> stripBeginAndEnd <*> firstCharLocation 'E' <*> zip (repeat 0) . charLocations 'a'
+
+main = print . (solution1 &&& solution2) . lines =<< readFile "input"
