1b78649408e46739482c81434beb439de45ea6da
l <info@datas.ink>
2022-12-13 17:29:50 +0000

    11++

--- 11/11.hs
+++ 11/11.hs
@@ -3,14 +3,13 @@ import Control.Arrow
 import Control.Lens
 import Control.Monad
 import Data.Bool
+import Data.IntMap.Lazy(IntMap)
 import Data.List
 import Data.List.Split
-import Data.Map(Map, (!))
 import Data.Maybe
 import Data.String.Utils
 import System.IO
-import qualified Data.Map as Map
-import Debug.Trace
+import qualified Data.IntMap.Lazy as IntMap
 
 
 data Monkey = Monkey { op' :: Int -> Int, next :: Int -> Int, divN :: Int }
@@ -35,26 +34,29 @@ readNext xs = bool falseCase trueCase . (== 0) . (`mod` n) where
 readDivN :: [String] -> Int
 readDivN = read . last . words . (!!3)
 
-startingItems :: [String] -> [Int]
-startingItems = map read . words . filter (`elem` "0123456789 ") . (!!1)
+readStartingItems :: [String] -> [Int]
+readStartingItems = map read . words . filter (`elem` "0123456789 ") . (!!1)
 
 readMonkey :: [String] -> ([Int], Monkey)
-readMonkey = startingItems &&& (Monkey <$> readOperation <*> readNext <*> readDivN)
+readMonkey = readStartingItems &&& (Monkey <$> readOperation <*> readNext <*> readDivN)
 
 
-processMonkey :: (Int -> Int) -> Int -> Monkey -> [Int] -> [[Int]]
-processMonkey worryControl n monkey = foldl (flip (\(a, b) -> over (element a) (++[b]))) (replicate n []) . map ((next monkey &&& id) . worryControl . op' monkey)
+processMonkey :: (Int -> Int) -> Int -> Monkey -> [Int] -> IntMap [Int]
+processMonkey worryControl n monkey = foldl foldFn IntMap.empty . map ((next monkey &&& id) . worryControl . op' monkey) where
+    foldFn curr (newMonkey, newVal) = IntMap.insertWith (flip (++)) newMonkey [newVal] curr
 
-doRound :: (Int -> Int) -> Int -> [(Int, Monkey)] -> ([[Int]], [Int]) -> ([[Int]], [Int])
-doRound worryControl n [] (arrs, counts) = (arrs, counts)
-doRound worryControl n ((idx, monkey):xs) (arrs, counts) = doRound worryControl n xs (flip (zipWith (++)) (processMonkey worryControl n monkey $ arrs!!idx) $ (element idx .~ []) arrs, over (element idx) (+ length (arrs!!idx)) counts)
+doRound :: (Int -> Int) -> Int -> [(Int, Monkey)] -> (IntMap [Int], [Int]) -> (IntMap [Int], [Int])
+doRound worryControl n [] = id
+doRound worryControl n ((idx, monkey):xs) = doRound worryControl n xs . (updateItems &&& updateInteractions) where
+    updateItems = (IntMap.unionWith (++) <$> processMonkey worryControl n monkey . (IntMap.! idx) <*> IntMap.insert idx []) . fst
+    updateInteractions = uncurry (over (element idx) . (+) . length . (IntMap.! idx))
 
-monkeyBusiness :: ([[Int]], [Int]) -> Int
+monkeyBusiness :: (a, [Int]) -> Int
 monkeyBusiness = product . take 2 . sortBy (flip compare) . snd
 
 
-monkeys :: String -> (([[Int]], [Int]), [Monkey])
-monkeys = first (id &&& flip replicate 0 . length) . unzip . map (readMonkey . lines) . splitOn "\n\n"
+monkeys :: String -> ((IntMap [Int], [Int]), [Monkey])
+monkeys = first (IntMap.fromAscList . zip [0..] &&& flip replicate 0 . length) . unzip . map (readMonkey . lines) . splitOn "\n\n"
 
 solution1 = monkeyBusiness . (!!20) . (iterate <$> (doRound (`div` 3) <$> length <*> zip [0..]) . snd <*> fst)
 solution2 = monkeyBusiness . (!!10000) . (iterate <$> (doRound <$> flip mod . foldl lcm 1 . map divN <*> length <*> zip [0..]) . snd <*> fst)
